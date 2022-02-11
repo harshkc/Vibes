36849a9874ac0bc9371be0fedc8a327bea6f7b09
harshkc <harsh.hkc1006@gmail.com>
2022-02-12 01:08:33 +0530

    adds logout method and refactoring

---
+++ src/utils/defaultStations.js
@@ -0,0 +1,42 @@
+export const defaultStations = [
+  {
+    video: "https://www.youtube.com/watch?v=5qap5aO4i9A&ab_channel=LofiGirl",
+    channelName: "Lofi Girl",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=7NOSDKb0HlU&ab_channel=ChillhopMusic",
+    channelName: "Chill Hop Music",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=dIdb_BWkZq4&ab_channel=AmbientRenders",
+    channelName: "Ambient Renders",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=j28Oyq6NnOs&ab_channel=IvyStationRecords",
+    channelName: "Ivy Records",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=w3LWHIz3bMc&ab_channel=nostalgic",
+    channelName: "Anime Vibe",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=kgx4WGK0oNU&ab_channel=%E9%98%BF%E9%B2%8DAbao",
+    channelName: "CloudHop",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=-9gEgshJUuY&ab_channel=lofigeek",
+    channelName: "H5G1 Music",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=uxR_sTZnBtg&ab_channel=StudyMD",
+    channelName: "StudyMD",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=xxgxkjV70Vc&ab_channel=NightrideFM",
+    channelName: "Astral Throb",
+  },
+  {
+    video: "https://www.youtube.com/watch?v=-5KAN9_CzSA&ab_channel=STEEZYASFUCK",
+    channelName: "theJazz Cafe",
+  },
+];
